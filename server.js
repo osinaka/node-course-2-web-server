b67092d074f12baf32611a5ec237fe4b7893c48e
osinaka <joyj8881@gmail.com>
2018-07-06 12:35:42 +0100

    Add project page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 // /bad - send back json with errormessage
 app.get('/bad', (req, res) => {
     res.send ({
